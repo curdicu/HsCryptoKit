42d5695cfa912a6ebee59533412fb735828c74af
curdicu <curdicu@gmail.com>
2025-02-28 21:45:12 +0800

    *

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/attaswift/BigInt.git", .upToNextMajor(from: "5.0.0")),
-        .package(url: "https://github.com/curdicu/HsExtensions.git", .branch("main")),
+        .package(url: "https://github.com/horizontalsystems/HsExtensions.Swift.git", .upToNextMajor(from: "1.0.0")),
         .package(url: "https://github.com/apple/swift-crypto.git", "1.0.0" ..< "3.0.0"),
         .package(url: "https://github.com/GigaBitcoin/secp256k1.swift.git", exact: .init(0, 10, 0))
     ],
@@ -29,7 +29,7 @@ let package = Package(
             dependencies: [
                 .target(name: "HsCryptoKitC"),
                 .product(name: "BigInt", package: "BigInt"),
-                .product(name: "HsExtensions", package: "HsExtensions"),
+                .product(name: "HsExtensions", package: "HsExtensions.Swift"),
                 .product(name: "Crypto", package: "swift-crypto"),
                 .product(name: "_CryptoExtras", package: "swift-crypto"),
                 .product(name: "secp256k1", package: "secp256k1.swift"),
